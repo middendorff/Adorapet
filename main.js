fba0ae6184099a0acd2df1bca1a6e6761d5c581e
middendorff <mike.b.perreault@gmail.com>
2022-03-15 18:55:25 -0400

    Uploading Initial Files
    The base files to start the project

---
+++ main.js
@@ -0,0 +1,16 @@
+function onload(){
+    let animating = false;
+    let petting = false;
+    let eating = fakse;
+    let idle = true;
+    const pet = document.createElement('div');
+    updatePosition(100, 100);
+    pet.classList.add('pet');
+    document.body.appendChild(pet);
+}
+
+
+function main(){
+
+
+}
---
+++ main.ssh.js
@@ -0,0 +1,9 @@
+.pet {
+    height: 300px;
+    width: 300px;
+    position: fixed;
+    z-index: 1000;
+    background-image: 
+    background-size: 30px 30px;
+    background-position: 0 0;
+  }
