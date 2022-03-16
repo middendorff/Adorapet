e655edbafc79052c87823e3c82d6a594b4dc420e
Mike <mike.b.perreault@gmail.com>
2022-03-15 22:02:47 -0400

    New pet files

---
+++ petBaseClass.js
@@ -0,0 +1,25 @@
+class PetBaseClass {
+    constructor(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel){
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.hungerLevel = hungerLevel;
+        this.happinessLevel = happinessLevel;
+    }
+    constructor(){
+        this.xCoord = 0;
+        this.yCoord = 0;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.hungerLevel = 0;
+        this.happinessLevel = 0;
+    }
+        pet() {
+
+        };
+        feed() {
+
+        };
+
+    }
---
+++ petClasses.js
@@ -0,0 +1,46 @@
+class bird extends PetBaseClass{
+    constructor(){
+        super();
+        this.name = "bird";
+        this.image = "";
+        this.imageRight = "";
+    }
+
+    constructor(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel){
+        super(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel);
+        this.name = "bird";
+        this.image = "";
+        this.imageRight = "";
+    }
+}
+
+class dog extends PetBaseClass{
+    constructor(){
+        super();
+        this.name = "dog";
+        this.image = "";
+        this.imageRight = "";
+    }
+
+    constructor(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel){
+        super(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel);
+        this.name = "dog";
+        this.image = "";
+        this.imageRight = "";
+    }
+}
+
+class cat extends PetBaseClass{
+    constructor(){
+        super();
+        this.name = "cat";
+        this.imageLeft = "";
+        this.imageRight = "";
+    }
+    constructor(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel){
+        super(xCoord,yCoord,xVel,yVel,hungerLevel,happinessLevel);
+        this.name = "cat";
+        this.image = "";
+        this.imageRight = "";
+    }
+}
